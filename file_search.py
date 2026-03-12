2f74a26958d114c097e6c69c2cf157acba9f5f9c
Redstone82 <hallo12345678321@gmail.com>
2026-03-12 20:56:15 +0100

    clean up 2

--- file_search.py
+++ file_search.py
@@ -1,8 +1,6 @@
 import os, time
 
-# os , time should be pre installed
-
-start_path = "/storage/emulated/0/"  # input your preset path examples:
+start_path = "/home/user-name/"  # input your preset path examples:
 
 liste = []
 # android = /storage/emulated/0/
@@ -16,6 +14,7 @@ def search_file(search, path):
 
         for root, dirs, files in os.walk(path):
             n += 1
+
             if search in files:
                 print("siblings: ")  # files that are on the same path and are folders
                 for file in files:
@@ -38,17 +37,19 @@ def search_file(search, path):
                 print("path: ", root)
                 print("it took ", clock, "s")
                 print("tries: ", n)
+
                 if input("do you want to continue searching? [Y/N]").lower() == "y":
                     liste.insert(0, "path: " + root + "\n" + "it took " + str(clock) + "s" + "\n" + "tryes: " + str(n) + "\n")
+                    n = 0
                 else:
                     print("\n", "-" * 35)
                     for listen in liste:
                         print(listen)
                     return None
-                return None  # else it would print the last line
 
         print(f"could not find file ({search})")
         return None
+
     else:
         print(f"'{path}' does not exist")
         return None
@@ -57,11 +58,13 @@ def search_file(search, path):
 def search_folder(search, path):
     start = time.time()  # starts the timer
     n = 0
+
     if os.path.isdir(path) or os.path.isfile(path):  # checks if the path from the user does exist
 
         for root, dirs, files in os.walk(path):  # goes through the path
             n += 1
-            if search in files:
+
+            if search in dirs:
                 print("siblings: ")  # files that are on the same path and are folders
                 for file in files:
                     if file == search:
@@ -83,24 +86,26 @@ def search_folder(search, path):
                 print("path: ", root)
                 print("it took ", clock, "s")
                 print("tries: ", n)
+
                 if input("do you want to continue searching? [Y/N]").lower() == "y":
                     liste.insert(0, "path: " + root + "\n" + "it took " + str(clock) + "s" + "\n" + "tryes: " + str(n) + "\n")
+                    n = 0
                 else:
                     print("\n", "-" * 35)
                     for listen in liste:
                         print(listen)
                     return None
-                return None  # else it would print the last line
 
         print(f"could not find file ({search})")
         return None
+
     else:
         print(f"{path} does not exist")
         return None
 
 
 def info():
-    inp_info = input("input a dir[1] or file[2]\n")
+    inp_info = input("input a dir[1] or file[2] or Exit[3]\n")
 
     if inp_info == "1":
         file_name = input("input the name of the folder\n")
@@ -113,6 +118,8 @@ def info():
         path_i = input(f"input a path else press enter\n`{start_path}` +") or start_path
 
         search_file(file_name, path_i)
+    elif inp_info == "3":
+        quit("bye")
     else:
         print("try again")
         return None
